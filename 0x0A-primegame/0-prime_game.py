557b0a71c5a2aaa4cb416e4cba12fe7b7b6e95ab
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-09-06 11:54:11 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,28 @@
 #!/usr/bin/python3
-
 """Winner Prime"""
 
 def isWinner(x, nums):
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
-
-    def can_win(n):
-        # If n is even, Maria wins; otherwise, Ben wins
-        return n % 2 == 0
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if is_prime(n):
-            if can_win(n):
-                maria_wins += 1
-            else:
-                ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    """Func._ that performs prime game"""
+    if not nums or x < 1:
         return None
-
-# Test the function with the given example
-x = 3
-nums = [4, 5, 1]
-result = isWinner(x, nums)
-print(result)  # Output should be "Ben" as Ben wins 2 rounds and Maria wins 1 round.
-
+    n = max(nums)
+    tcase = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not tcase[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            tcase[j] = False
+    tcase[0] = tcase[1] = False
+    c = 0
+    for i in range(len(tcase)):
+        if tcase[i]:
+            c += 1
+        tcase[i] = c
+    player1 = 0
+    for n in nums:
+        player1 += tcase[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
